f19f816a98debf50b30ba1559a231e354119b541
paranacidadeADS <ads.projeto.mi@gmail.com>
2023-03-20 01:29:41 -0300

    v4

--- js/script.js
+++ js/script.js
@@ -1,73 +1,23 @@
-const apoio = document.querySelector(".texto-apoio");
-const texto = document.querySelector(".texto");
-const numquest = document.querySelector(".num-quest");
-const question = document.querySelector(".question");
-const answers = document.querySelector(".answers");
-const spnQtd = document.querySelector(".spnQtd");
-const textFinish = document.querySelector(".finish span");
-const content = document.querySelector(".content");
-const contentFinish = document.querySelector(".finish");
-const btnRestart = document.querySelector(".finish button");
-const proximo = document.querySelector(".proximo");
-
-import questions from "./questions.js";
-
-let currentIndex = 0;
-let questionsCorrect = 0;
-
-btnRestart.onclick = () => {
-  content.style.display = "flex";
-  contentFinish.style.display = "none";
-
-  currentIndex = 0;
-  questionsCorrect = 0;
-  loadQuestion();
-};
-
-function nextQuestion(e) {
-  if (e.target.getAttribute("data-correct") === "true") {
-    questionsCorrect++;
+function verificarRespostas() {
+  let acertos = 0; // Aqui estou declarando uma variável de valor 0. pq? PQ EU QUIS PORRA
+  
+  // Questão 1
+  const resposta1 = document.querySelector('input[name="q1"]:checked').value;
+  if (resposta1 === "b") {  // TA VENDO ESSA MERDA AQ? ISSO AQ É UMA ESTRUTURA CONDICIONAL
+    acertos++; // E essa porra é um contador, é bem importante (e me salva muito), dê um olá ao contador! Ele com certeza te salvará depois.
   }
-
-  if (currentIndex < questions.length - 1) {
-    currentIndex++;
-    loadQuestion();
-  } else {
-    finish();
+  
+  // Questão 2
+  const resposta2 = document.querySelector('input[name="q2"]:checked').value;
+  if (resposta2 === "b") {
+    acertos++;
   }
+  
+  // Questão 3
+  const resposta3 = document.querySelector('input[name="q3"]:checked').value;
+  if (resposta3 === "a") {
+    acertos++;
+  }
+  
+  alert(`Você acertou ${acertos} questões.`); //Retorno para o usuário.      
 }
-
-function finish() {
-  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
-  content.style.display = "none";
-  contentFinish.style.display = "flex";
-}
-
-function loadQuestion() {
-  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
-  const item = questions[currentIndex];
-  apoio.innerHTML = item.apoio;
-  texto.innerHTML = item.texto;
-  numquest.innerHTML = item.numquest;
-  answers.innerHTML = "";
-  question.innerHTML = item.question;
-
-  item.answers.forEach((answer) => {
-    const div = document.createElement("div");
-
-    div.innerHTML = `
-    <label>
-    <input type="radio" class="answer" data-correct="${answer.correct}">
-      ${answer.option}
-    </label>
-    `;
-
-    answers.appendChild(div);
-  });
-
-  document.querySelectorAll(".answer").forEach((item) => {
-    item.addEventListener("click", nextQuestion);
-  });
-}
-
-loadQuestion();
